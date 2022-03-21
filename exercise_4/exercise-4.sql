6314ba535c75559d36aa363c67670e051ef16c42
Chase Crowder <gcrowder93@gmail.com>
2022-03-21 14:25:53 -0700

    test 5 passing. first try was backwards

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,7 +1,6 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
 
-
 -- customer/address shares address_id
 -- address/city shares city_id
 
--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,36 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+
+-- film_id
+-- actor_id
+
+SELECT
+title
+FROM
+film
+LEFT JOIN
+film_actor
+ON
+film.film_id = film_actor.film_id
+LEFT JOIN
+actor
+ON
+film_actor.actor_id = actor.actor_id
+WHERE
+last_name = 'Davis'
+
+
+-- SELECT
+-- last_name
+-- FROM
+-- actor
+-- LEFT JOIN
+-- film_actor
+-- ON
+-- actor.actor_id = film_actor.actor_id
+-- LEFT JOIN
+-- film
+-- ON
+-- film_actor.film_id = film.film_id
+-- WHERE
+-- last_name = 'Davis'
